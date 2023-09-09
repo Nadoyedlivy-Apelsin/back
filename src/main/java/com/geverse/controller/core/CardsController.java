16b9a571e3b6c76508b1713f24294642c2fb4a1c
Matik01 <piupip4@gmail.com>
2023-09-09 03:36:31 +0300

    ff

--- src/main/java/com/geverse/controller/core/CardsController.java
+++ src/main/java/com/geverse/controller/core/CardsController.java
@@ -2,6 +2,7 @@ package com.geverse.controller.core;
 
 import com.geverse.entity.GeneratedSequence;
 import com.geverse.service.GeneratedSequenceService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class CardsController {
     public CardsController(GeneratedSequenceService service) {
         this.service = service;
     }
-    @RequestMapping("/get_generated")
+    @GetMapping("/get_generated")
     public List<GeneratedSequence> transferGeneratedProtein() {
         return service.findAll();
     }
